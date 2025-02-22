c819196750a540fa4a765f5bd48f2cb9ca5a1cfe
Jeff Culverhouse <jeff@graystorm.com>
2025-02-21 21:27:39 -0500

    cleanup, newer base image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9-alpine
+FROM python:3.14.0a5-alpine3.21
 
 RUN python3 -m ensurepip
 
--- app.py
+++ app.py
@@ -21,39 +21,28 @@ try:
     with open(configdir + "config.yaml") as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
 except:
-    config = {}
-    config['mqtt'] = {}
-    config['mqtt']['host'] = os.getenv("MQTT_HOST")
-    config['mqtt']['qos'] = int(os.getenv("MQTT_QOS") or 0)
-    config['mqtt']['port'] = int(os.getenv("MQTT_PORT") or 1883)
-    config['mqtt']['username'] = os.getenv("MQTT_USERNAME")
-    config['mqtt']['password'] = os.getenv("MQTT_PASSWORD")  # can be None
-
-    config['mqtt']['prefix'] = os.getenv("MQTT_PREFIX")
-    config['mqtt']['homeassistant'] = os.getenv("MQTT_HOMEASSISTANT")
-
-    config['mqtt']['tls_enabled'] = os.getenv("MQTT_TLS_ENABLED") == "true"
-    config['mqtt']['tls_ca_cert'] = os.getenv("MQTT_TLS_CA_CERT")
-    config['mqtt']['tls_cert'] = os.getenv("MQTT_TLS_CERT")
-    config['mqtt']['tls_key'] = os.getenv("MQTT_TLS_KEY")
-
-    config['govee'] = {}
-    config['govee']['api_key'] = os.getenv("GOVEE_API_KEY")
-    config['govee']['device_interval'] = int(os.getenv("GOVEE_DEVICE_INTERVAL"))
-    config['govee']['device_boost_interval'] = int(os.getenv("GOVEE_DEVICE_BOOST_INTERVAL"))
-    config['govee']['device_list_interval'] = int(os.getenv("GOVEE_LIST_INTERVAL"))
-
-    config['debug'] = True if os.getenv("GOVEE_DEBUG") else False
-
-# fallback defaults
-config['mqtt'].setdefault('host','localhost')
-config['mqtt'].setdefault('qos', 0)
-config['mqtt'].setdefault('port', 1883)
-config['mqtt'].setdefault('prefix', 'govee')
-config['mqtt'].setdefault('homeassistant', 'homeassistant')
-config['govee'].setdefault('device_interval', 30)
-config['govee'].setdefault('device_boost_interval', 5)
-config['govee'].setdefault('device_list_interval', 300)
+    config = {
+        mqtt: {
+            host: os.genenv("MQTT_HOST") or 'localhost',
+            qos: int(os.getenv("MQTT_QOS") or 0),
+            port: int(os.getenv("MQTT_PORT") or 1883),
+            username: os.getenv("MQTT_USERNAME"),
+            password: os.getenv("MQTT_PASSWORD"),  # can be None
+            prefix: os.getenv("MQTT_PREFIX") or 'govee',
+            homeassistant: os.getenv("MQTT_HOMEASSISTANT") or 'homeassistant',
+            tls_enabled: os.getenv("MQTT_TLS_ENABLED") == "true",
+            tls_ca_cert: os.getenv("MQTT_TLS_CA_CERT"),
+            tls_cert: os.getenv("MQTT_TLS_CERT"),
+            tls_key: os.getenv("MQTT_TLS_KEY"),
+        },
+        govee: {
+            api_key: os.getenv("GOVEE_API_KEY"),
+            device_interval: int(os.getenv("GOVEE_DEVICE_INTERVAL") or 30),
+            device_boost_interval: int(os.getenv("GOVEE_DEVICE_BOOST_INTERVAL") or 5),
+            device_list_interval: int(os.getenv("GOVEE_LIST_INTERVAL") or 3600),
+        },
+        debug: True if os.getenv("GOVEE_DEBUG") else False,
+    }
 
 if 'debug' in config and config['debug'] is True:
     logging.basicConfig(level=logging.DEBUG)
